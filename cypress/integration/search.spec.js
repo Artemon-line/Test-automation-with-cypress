d29bdb900f12ce77ac39db15cde627fbe1817381
artemon-line <tyran77757@gmail.com>
2019-02-09 15:50:23 +0200

    add login test

--- cypress/integration/search.spec.js
+++ cypress/integration/search.spec.js
@@ -1,10 +1,12 @@
-context('Test npm search', () => {
+const LoginPage = require('../pages/login.page');
+
+context('Test search', () => {
   beforeEach(() => {
     cy.visit('');
   });
   it('test search field', () => {
     //
-    console.log('ssssss');
+    cy.log('test begin');
     cy.get('.header__searchInput___102CH').type('cypress');
     cy.get('.header__searchButton___2AVlX').click();
     cy.get('[href="/signup"]')
@@ -13,5 +15,17 @@ context('Test npm search', () => {
     cy.get(
       ':nth-child(1) > .w-80 > .package-list-item__description___1nEpN'
     ).should('have.text', 'Cypress.io end to end testing tool');
+  });
+
+  it('Test login with wrong values', () => {
+    //
+    cy.visit(LoginPage.path);
+    cy.get(LoginPage.usernameInput).type('TESTTTTT');
+    cy.get(LoginPage.passwordInput).type('TestPassword');
+    cy.get('.forms__buttonGradient___wj4os').click();
+    cy.get('.notifications__notification___3wwtp > :nth-child(1)').should(
+      'have.text',
+      'username or password was invalid'
+    );
   });
 });
---
+++ cypress/pages/login.page.js
@@ -0,0 +1,6 @@
+const LoginPage = function() {
+  this.usernameInput = '#login_username';
+  this.passwordInput = '#login_password';
+  this.path = '/login';
+};
+module.exports = new LoginPage();
